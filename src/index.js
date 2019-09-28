c87fa7d56be5503c3d19173c355cfe2b35fb6b5b
Nilson <nilsontechh@gmail.com>
2019-09-27 22:06:16 -0300

    Refreshing the port!

--- src/index.js
+++ src/index.js
@@ -17,7 +17,9 @@ const vehicles = require('./api/vehicles')
 app.use('/customers', customers);
 app.use('/vehicles', vehicles);
 
+const PORT = process.env.PORT || 3000;
+
 // RODANDO O APP
-app.listen(3000, function () {
-  console.log('Estou rodando na porta 3000!');
+app.listen(PORT, function () {
+  console.log('Estou rodando na porta ${PORT}!');
 });
