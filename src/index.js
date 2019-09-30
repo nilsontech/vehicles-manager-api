5d92d3237e9cc3d0e48a1b3dc43b89094253da4b
Nilson <nilsontechh@gmail.com>
2019-09-30 20:01:30 -0300

    Adding 'cors pack'.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
 
 app.use(bodyParser.json());
+app.use('*');
 
 // DECLARANDO A ROTA
 // app.get('/', function (req, res) {
