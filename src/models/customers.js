3d50f1cd0b22d2862b913a8278854884d8670a1d
Nilson <nilsontechh@gmail.com>
2019-09-30 21:50:36 -0300

    Updating new customer's data

--- src/models/customers.js
+++ src/models/customers.js
@@ -2,7 +2,15 @@ const db = require('../db');
 
 const customers = db.model('customers', {
   name: String,
+  cpf: String,
+  cep: Number,
   street: String,
+  number: String,
+  adjunct: String,
+  neighborhood: String,
+  city: String,
+  state: String,
+  obs: String,
   created: { type: Date, default: Date.now }
 });
 
